31e641a08c7961e4f7df93664b18f0300c907e26
Amrit Raj <141901677+theamritraj@users.noreply.github.com>
2025-05-08 00:46:14 +0530

    code

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MainPage from './Components/MainPage'
+
+function App() {
+ 
+
+  return (
+    <>
+
+      <MainPage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/MainPage.jsx
@@ -0,0 +1,14 @@
+
+const MainPage = ()=>{
+  return(
+    <>
+    <div className="container">
+      <div className="searchBar">
+        <input type="text" placeholder="Enter Dishes"/>
+        <button>Search</button>
+      </div>
+    </div>
+    </>
+  )
+}
+export default MainPage;
